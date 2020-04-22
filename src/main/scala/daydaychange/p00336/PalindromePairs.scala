a2aad6f2c1bcf52abdb73cb538614f7558f5f821
fjl716 <fjl716@163.com>
2020-04-22 18:11:38 +0800

    增加另一种算法

--- src/main/scala/daydaychange/p00336/PalindromePairs.scala
+++ src/main/scala/daydaychange/p00336/PalindromePairs.scala
@@ -1,19 +1,77 @@
 package daydaychange.p00336
 
 object PalindromePairs {
-  def find(values: String*): Seq[(Int, Int)] = {
-    for (i <- 0 until values.length; j <- 0 until values.length if i != j && test(values(i),values(j))) yield {
-      i -> j
-    }
-  }
+  var index = 0
 
-  private def test(val1: String, val2: String): Boolean = {
-    val s = val1 + val2
-    for (i <- 0 until s.length / 2) {
-      if (s(i) != s(s.length - i - 1)) {
+  private def isPalindrome(word: String): Boolean = {
+    index = index + 1
+    for (i <- 0 until word.length / 2) {
+      if (word(i) != word(word.length - i - 1)) {
         return false
       }
     }
     true
   }
+
+  def find1(words: String*): Seq[(Int, Int)] = {
+    index = 0
+    val result = for (i <- 0 until words.length; j <- 0 until words.length if i != j && isPalindrome(words(i) + words(j))) yield {
+      i -> j
+    }
+    println(s"find1 ${words.length} ${index}")
+    result
+  }
+
+  private def isPalindromeBetween(word: String, front: Int, back: Int): Boolean = {
+    index = index + 1
+    for (i <- 0 until (back - front)) {
+      if (word(front + i) != word(back - i)) {
+        return false
+      }
+    }
+    true
+  }
+
+  def allValidPrefixes(word: String): Seq[String] = {
+    for (i <- 0 until word.length if isPalindromeBetween(word, i, word.length() - 1)) yield {
+      word.substring(0, i)
+    }
+  }
+
+  def allValidSuffixes(word: String): Seq[String] = {
+    for (i <- 0 until word.length if isPalindromeBetween(word, 0, i)) yield {
+      word.substring(i + 1, word.length)
+    }
+  }
+
+  def find2(words: String*): Seq[(Int, Int)] = {
+    index = 0
+    var result: Seq[(Int, Int)] = Seq()
+    val wordSet = Map((for (i <- 0 until words.length) yield {
+      words(i) -> i
+    }): _*)
+
+    words.foreach(word => {
+      val currentWordIndex = wordSet(word)
+      val reversedWord = new StringBuilder(word).reverse.toString
+      if (wordSet.contains(reversedWord) && (wordSet(reversedWord) != currentWordIndex)) {
+        result = result :+ (currentWordIndex -> wordSet(reversedWord))
+      }
+      allValidSuffixes(word).foreach(suffix => {
+        val reversedSuffix = new StringBuilder(suffix).reverse.toString
+        if (wordSet.contains(reversedSuffix)) {
+          result = result :+ (wordSet(reversedSuffix) -> currentWordIndex)
+        }
+      })
+      allValidPrefixes(word).foreach(suffix => {
+        val reversedSuffix = new StringBuilder(suffix).reverse.toString
+        if (wordSet.contains(reversedSuffix)) {
+          result = result :+ (currentWordIndex -> wordSet(reversedSuffix))
+        }
+      })
+    })
+    println(s"find2 ${words.length} ${index}")
+    result
+  }
+
 }
--- src/test/scala/daydaychange/p00336/PalindromePairsUnit.scala
+++ src/test/scala/daydaychange/p00336/PalindromePairsUnit.scala
@@ -4,8 +4,13 @@ import org.specs2.mutable.Specification
 
 class PalindromePairsUnit extends Specification {
   "LongestPalindrome" should {
-    PalindromePairs.find("abcd", "dcba", "lls", "s", "sssll") mustEqual Seq(0 -> 1, 1 -> 0, 2 -> 4, 3 -> 2)
-    PalindromePairs.find("bat", "tab", "cat") mustEqual Seq(0 -> 1, 1 -> 0)
-    "" mustEqual ""
+//    PalindromePairs.find1("abcd", "dcba", "lls", "s", "sssllaaasdfasqwvcasdrqwtbsdfwertqczzdfqw4rtyrnx")
+//    PalindromePairs.find2("abcd", "dcba", "lls", "s", "sssllaaasdfasqwvcasdrqwtbsdfwertqczzdfqw4rtyrnx")
+
+
+    PalindromePairs.find1("abcd", "dcba", "lls", "s", "sssll") mustEqual Seq(0 -> 1, 1 -> 0, 2 -> 4, 3 -> 2)
+    PalindromePairs.find2("abcd", "dcba", "lls", "s", "sssll") mustEqual Seq(0 -> 1, 1 -> 0, 3 -> 2, 2 -> 4)
+    PalindromePairs.find1("bat", "tab", "cat") mustEqual Seq(0 -> 1, 1 -> 0)
+    PalindromePairs.find2("bat", "tab", "cat") mustEqual Seq(0 -> 1, 1 -> 0)
   }
 }
