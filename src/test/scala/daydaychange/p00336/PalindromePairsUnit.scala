999aa53c1591637c78ec71cefbd72376642471ad
fjl716 <fjl716@163.com>
2020-04-23 09:15:13 +0800

    更新新算法单元测试，新算法对多字符串测试效率上确实有提升。

--- src/test/scala/daydaychange/p00336/PalindromePairsUnit.scala
+++ src/test/scala/daydaychange/p00336/PalindromePairsUnit.scala
@@ -4,9 +4,16 @@ import org.specs2.mutable.Specification
 
 class PalindromePairsUnit extends Specification {
   "LongestPalindrome" should {
-//    PalindromePairs.find1("abcd", "dcba", "lls", "s", "sssllaaasdfasqwvcasdrqwtbsdfwertqczzdfqw4rtyrnx")
-//    PalindromePairs.find2("abcd", "dcba", "lls", "s", "sssllaaasdfasqwvcasdrqwtbsdfwertqczzdfqw4rtyrnx")
+    //    PalindromePairs.find1("abcd", "dcba", "lls", "s", "sssllaaasdfasqwvcasdrqwtbsdfwertqczzdfqw4rtyrnx")
+    //    PalindromePairs.find2("abcd", "dcba", "lls", "s", "sssllaaasdfasqwvcasdrqwtbsdfwertqczzdfqw4rtyrnx")
 
+    PalindromePairs.find1("abcd", "dcba", "lls", "s", "sssll",
+      "1111", "2222", "3333", "4444", "5555", "6666", "7777", "8888", "9999", "00000",
+      "AAAA", "BBBB",
+    ) mustEqual Seq(0 -> 1, 1 -> 0, 2 -> 4, 3 -> 2)
+    PalindromePairs.find2("abcd", "dcba", "lls", "s", "sssll",
+      "1111", "2222", "3333", "4444", "5555", "6666", "7777", "8888", "9999", "00000",
+      "AAAA", "BBBB") mustEqual Seq(0 -> 1, 1 -> 0, 3 -> 2, 2 -> 4)
 
     PalindromePairs.find1("abcd", "dcba", "lls", "s", "sssll") mustEqual Seq(0 -> 1, 1 -> 0, 2 -> 4, 3 -> 2)
     PalindromePairs.find2("abcd", "dcba", "lls", "s", "sssll") mustEqual Seq(0 -> 1, 1 -> 0, 3 -> 2, 2 -> 4)
