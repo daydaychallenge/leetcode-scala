b0091d806bcbfc30f751205c47f188f60df6486c
fjl716 <fjl716@163.com>
2020-04-30 11:23:56 +0800

    CountandSay

--- src/test/scala/daydaychange/p00038/CountandSayUnit.scala
+++ src/test/scala/daydaychange/p00038/CountandSayUnit.scala
@@ -6,5 +6,7 @@ class CountandSayUnit extends Specification {
   "CountandSayUnit" should {
     CountandSay.countAndSay(1) mustEqual "1"
     CountandSay.countAndSay(4) mustEqual "1211"
+    CountandSay.countAndSay2(1) mustEqual "1"
+    CountandSay.countAndSay2(4) mustEqual "1211"
   }
 }
